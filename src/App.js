35d9087c740f978a83b0f5923bc75a4cfe0a6d50
V. Lai <lai.vivian@gmail.com>
2017-07-25 15:04:17 -0700

    update portfoliio

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { tealA700 } from 'material-ui/styles/colors';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import TopAppBar from './TopAppBar';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+const muiTheme = getMuiTheme({
+  appBar: {
+    height: 70,
+    color: tealA700,
+  },
+});
+
+const App = () => (
+  <MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
+    <TopAppBar />
+  </MuiThemeProvider>
+);
 
 export default App;
