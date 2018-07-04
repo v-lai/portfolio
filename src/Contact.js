b7ac5846670555cdf86d57e6eae9111c2bd08015
V. Lai <lai.vivian@gmail.com>
2018-07-03 19:57:30 -0700

    update info

--- src/Contact.js
+++ src/Contact.js
@@ -5,20 +5,12 @@ const Contact = () => (
   <div className="innerBody">
     <h2>Contact</h2>
     <div className="space">
-    <a href="mailto:lai.vivian@gmail.com" alt="Email">
-      <i className="contact-fa fa fa-envelope-square" aria-hidden="true"></i>
-    </a>
-    &nbsp;
     <a href="https://github.com/v-lai" alt="GitHub">
       <i className="devicon devicon-github-plain"></i>
     </a>
     &nbsp;
     <a href="https://linkedin.com/in/laivivian" alt="LinkedIn">
       <i className="contact-fa fa fa-linkedin-square" aria-hidden="true"></i>
-    </a>
-    &nbsp;
-    <a href="https://angel.co/v-lai" alt="AngelList">
-      <i className="contact-fa fa fa-angellist" aria-hidden="true"></i>
     </a>
     </div>
   </div>
